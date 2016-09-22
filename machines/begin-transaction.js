01502e6e8a84c792f20336f73595bc5fd87a2fbf
Cody Stoltman <particlebanana@gmail.com>
2016-09-22 17:02:26 -0500

    clean up transactional interface

--- machines/begin-transaction.js
+++ machines/begin-transaction.js
@@ -12,7 +12,8 @@ module.exports = {
     connection: {
       friendlyName: 'Connection',
       description: 'An active database connection.',
-      extendedDescription: 'The provided database connection instance must still be active.  Only database connection instances created by the `getConnection()` machine in this driver are supported.',
+      extendedDescription: 'The provided database connection instance must still be active. Only database ' +
+        'connection instances created by the `getConnection()` machine in this driver are supported.',
       example: '===',
       required: true
     },
@@ -20,7 +21,8 @@ module.exports = {
     meta: {
       friendlyName: 'Meta (custom)',
       description: 'Additional stuff to pass to the driver.',
-      extendedDescription: 'This is reserved for custom driver-specific extensions.  Please refer to the documentation for the driver you are using for more specific information.',
+      extendedDescription: 'This is reserved for custom driver-specific extensions. Please refer to the ' +
+        'documentation for the driver you are using for more specific information.',
       example: '==='
     }
 
@@ -31,7 +33,9 @@ module.exports = {
 
     success: {
       description: 'The transaction was successfully started.',
-      extendedDescription: 'Until it is committed, rolled back, or times out, subsequent queries run on this connection will be transactional.  They will not have any true effect on the database until the transaction is committed, and will not affect queries made on other connections.',
+      extendedDescription: 'Until it is committed, rolled back, or times out, subsequent queries run on this ' +
+        'connection will be transactional. They will not have any true effect on the database until the transaction ' +
+        'is committed, and will not affect queries made on other connections.',
       outputVariableName: 'report',
       outputDescription: 'The `meta` property is reserved for custom driver-specific extensions.',
       example: {
@@ -41,10 +45,14 @@ module.exports = {
 
     badConnection: {
       friendlyName: 'Bad connection',
-      description: 'The provided connection is not valid or no longer active.  Are you sure it was obtained by calling this driver\'s `getConnection()` method?',
-      extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or timing issue in userland code.  In production, this can mean that the database became overwhelemed or was shut off while some business logic was in progress.',
+      description: 'The provided connection is not valid or no longer active. Are you sure it was obtained by ' +
+        'calling this driver\'s `getConnection()` method?',
+      extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or ' +
+        'timing issue in userland code. In production, this can mean that the database became overwhelemed or was shut ' +
+        'off while some business logic was in progress.',
       outputVariableName: 'report',
-      outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom driver-specific extensions.',
+      outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database. ' +
+        'The `meta` property is reserved for custom driver-specific extensions.',
       example: {
         error: '===',
         meta: '==='
--- machines/rollback-transaction.js
+++ machines/rollback-transaction.js
@@ -15,7 +15,8 @@ module.exports = {
     connection: {
       friendlyName: 'Connection',
       description: 'An active database connection.',
-      extendedDescription: 'The provided database connection instance must still be active.  Only database connection instances created by the `getConnection()` machine in this driver are supported.',
+      extendedDescription: 'The provided database connection instance must still be active. Only database connection ' +
+        'instances created by the `getConnection()` machine in this driver are supported.',
       example: '===',
       required: true
     },
@@ -23,7 +24,8 @@ module.exports = {
     meta: {
       friendlyName: 'Meta (custom)',
       description: 'Additional stuff to pass to the driver.',
-      extendedDescription: 'This is reserved for custom driver-specific extensions.  Please refer to the documentation for the driver you are using for more specific information.',
+      extendedDescription: 'This is reserved for custom driver-specific extensions. Please refer to the documentation ' +
+        'for the driver you are using for more specific information.',
       example: '==='
     }
 
@@ -44,10 +46,14 @@ module.exports = {
 
     badConnection: {
       friendlyName: 'Bad connection',
-      description: 'The provided connection is not valid or no longer active.  Are you sure it was obtained by calling this driver\'s `getConnection()` method?',
-      extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or timing issue in userland code.  In production, this can mean that the database became overwhelemed or was shut off while some business logic was in progress.',
+      description: 'The provided connection is not valid or no longer active. Are you sure it was obtained by calling this ' +
+        'driver\'s `getConnection()` method?',
+      extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or timing issue ' +
+        'in userland code. In production, this can mean that the database became overwhelemed or was shut off while some business ' +
+        'logic was in progress.',
       outputVariableName: 'report',
-      outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom driver-specific extensions.',
+      outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database. ' +
+        'The `meta` property is reserved for custom driver-specific extensions.',
       example: {
         error: '===',
         meta: '==='
