86b4f938a1e83ebeb510ffa7e0eb928555377761
Scott Gress <scottmgress@gmail.com>
2017-01-31 18:05:11 -0600

    Update tests

--- test/integration/connectable/create-manager.js
+++ test/integration/connectable/create-manager.js
@@ -4,21 +4,43 @@ var Pack = require('../../../');
 
 describe('Connectable ::', function() {
   describe('Create Manager', function() {
-    it('should validate the connection string has a protocol', function(done) {
+    it('should work without a protocol in the connection string', function(done) {
       Pack.createManager({
         connectionString: 'localhost:5432/mppg'
       })
       .exec(function(err) {
-        assert(err);
-        assert.equal(err.exit, 'malformed');
+        if (err) {
+          return done(err);
+        }
+        return done();
+      });
+    });
+
+    it('should successfully return a PG Pool instance (using postgres:// protocol)', function(done) {
+      Pack.createManager({
+        connectionString: 'postgres://mp:mp@localhost:5432/mppg'
+      })
+      .exec(function(err, report) {
+        if (err) {
+          return done(err);
+        }
+
+        // Assert that the manager has a pool object
+        assert(report.manager.pool);
+
+        // Assert that a PG Pool is returned
+        assert(report.manager.pool instanceof pg.Pool);
+
+        // Assert that the manager has a connect function
+        assert(report.manager.pool.connect);
 
         return done();
       });
     });
 
-    it('should successfully return a PG Pool instance', function(done) {
+    it('should successfully return a PG Pool instance (using postgresql:// protocol)', function(done) {
       Pack.createManager({
-        connectionString: 'postgres://mp:mp@localhost:5432/mppg'
+        connectionString: 'postgresql://mp:mp@localhost:5432/mppg'
       })
       .exec(function(err, report) {
         if (err) {
