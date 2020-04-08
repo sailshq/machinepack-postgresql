8efaf82aaeb667832a7baf827a275e8b7937ee33
Vijay Konnackal <vijay.konnackal@gmail.com>
2020-04-08 17:57:28 +0530

    * hardcoded sqlserver password

--- test/docker/travis-test.sh
+++ test/docker/travis-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -i /machinepack-sqlserver/sqlserver.setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1 -i /machinepack-sqlserver/sqlserver.setup.sql
