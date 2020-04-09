f12d0a1be1e6cad734d52878d51aad2da37e748d
Vijay Konnackal <vijay.konnackal@gmail.com>
2020-04-09 15:16:07 +0530

    enabled waitfor it.4

--- test/docker/create-db.sh
+++ test/docker/create-db.sh
@@ -1,3 +1,3 @@
 
 /tmp/wait-for-it.sh localhost:1433 -t 120 -- echo 'sqlserver up'
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /tmp/sqlserver.setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1 -d master -i /tmp/sqlserver.setup.sql
