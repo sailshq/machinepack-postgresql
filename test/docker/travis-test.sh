6601f8d3c7067f34d8bee27f4e9d3b203300df98
Vijay Konnackal <vijay.konnackal@gmail.com>
2020-04-09 09:00:52 +0530

    * Travis env setting

--- test/docker/travis-test.sh
+++ test/docker/travis-test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1 -i /tmp/sqlserver.setup.sql
+echo `pwd`
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1 -i test/docker/sqlserver.setup.sql
