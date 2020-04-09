f0d6a2fe39c1c6bb82924caa03a642e80ae109c9
Vijay Konnackal <vijay.konnackal@gmail.com>
2020-04-09 14:11:21 +0530

    env for password

--- test/docker/create-db.sh
+++ test/docker/create-db.sh
@@ -1,2 +1,2 @@
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password1 -d master -i /tmp/sqlserver.setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /tmp/sqlserver.setup.sql
